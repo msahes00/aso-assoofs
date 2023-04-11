6f35a7cf27e0105994053ce0d30f7f4815d9513a
msahes00 <91277842+msahes00@users.noreply.github.com>
2023-04-11 18:53:08 +0200

    Save progress 11/04/23

--- assoofs.c
+++ assoofs.c
@@ -18,11 +18,13 @@ const struct file_operations assoofs_file_operations = {
 
 ssize_t assoofs_read(struct file * filp, char __user * buf, size_t len, loff_t * ppos) {
     printk(KERN_INFO "Read request\n");
+    // TODO
     return 0;
 }
 
 ssize_t assoofs_write(struct file * filp, const char __user * buf, size_t len, loff_t * ppos) {
     printk(KERN_INFO "Write request\n");
+    // TODO
     return 0;
 }
 
@@ -37,6 +39,7 @@ const struct file_operations assoofs_dir_operations = {
 
 static int assoofs_iterate(struct file *filp, struct dir_context *ctx) {
     printk(KERN_INFO "Iterate request\n");
+    // TODO
     return 0;
 }
 
@@ -54,17 +57,20 @@ static struct inode_operations assoofs_inode_ops = {
 
 struct dentry *assoofs_lookup(struct inode *parent_inode, struct dentry *child_dentry, unsigned int flags) {
     printk(KERN_INFO "Lookup request\n");
+    // TODO
     return NULL;
 }
 
 
 static int assoofs_create(struct user_namespace *mnt_userns, struct inode *dir, struct dentry *dentry, umode_t mode, bool excl) {
     printk(KERN_INFO "New file request\n");
+    // TODO
     return 0;
 }
 
 static int assoofs_mkdir(struct user_namespace *mnt_userns, struct inode *dir , struct dentry *dentry, umode_t mode) {
     printk(KERN_INFO "New directory request\n");
+    // TODO
     return 0;
 }
 
@@ -79,11 +85,28 @@ static const struct super_operations assoofs_sops = {
  *  Inicialización del superbloque
  */
 int assoofs_fill_super(struct super_block *sb, void *data, int silent) {   
+    // TODO: error checking  
     printk(KERN_INFO ASSOOFS_NAME "_fill_super request\n");
     // 1.- Leer la información persistente del superbloque del dispositivo de bloques  
+    struct buffer_head *bh = sb_bread(sb, ASSOOFS_SUPERBLOCK_BLOCK_NUMBER);
+    struct assoofs_super_block_info *assoofs_sb (struct assoofs_super_block_info *) bh->b_data;
     // 2.- Comprobar los parámetros del superbloque
+    if ((assoofs_sb->magic != ASSOOFS_MAGIC) || (assoofs_sb->block_size != ASSOOFS_DEFAULT_BLOCK_SIZE)) return -1; // TODO
     // 3.- Escribir la información persistente leída del dispositivo de bloques en el superbloque sb, incluído el campo s_op con las operaciones que soporta.
+    sb->s_magic = ASSOOFS_MAGIC;
+    sb->s_axbytes = ASSOOFS_DEFAULT_BLOCK_SIZE;
+
+    static const struct super_operations assoofs_sops = {
+        .drop_inode = generic_delete_inode,
+    };
+    sb->s_op = assoofs_sops;
+    sb->s_fs_info = assoofs_sb;
+
     // 4.- Crear el inodo raíz y asignarle operaciones sobre inodos (i_op) y sobre directorios (i_fop)
+    struct inode *root_inode = new_inode(sb);
+    inode_init_owner(sb->s_user_ns, root_inode, NULL, S_IFDIR);
+    
+    brelse(bh);
     return 0;
 }
 
@@ -95,6 +118,7 @@ static struct dentry *assoofs_mount(struct file_system_type *fs_type, int flags,
     printk(KERN_INFO ASSOOFS_NAME "_mount request\n");
     ret = mount_bdev(fs_type, flags, dev_name, data, assoofs_fill_super);
     // Control de errores a partir del valor de ret. En este caso se puede utilizar la macro IS_ERR: if (IS_ERR(ret)) ...
+    // TODO
     return ret;
 }
 
@@ -113,6 +137,7 @@ static int __init assoofs_init(void) {
     printk(KERN_INFO ASSOOFS_NAME "_init request\n");
     ret = register_filesystem(&assoofs_type);
     // Control de errores a partir del valor de ret
+    // TODO
     return ret;
 }
 
@@ -121,6 +146,7 @@ static void __exit assoofs_exit(void) {
     printk(KERN_INFO ASSOOFS_NAME "_exit request\n");
     ret = unregister_filesystem(&assoofs_type);
     // Control de errores a partir del valor de ret
+    // TODO
 }
 
 module_init(assoofs_init);
